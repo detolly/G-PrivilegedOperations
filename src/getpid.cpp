41065a06add873e8bea9f61991d7c49e58ceaef1
detolly <thomas@detolly.no>
2024-07-21 20:41:39 +0200

    getpid, modhost

---
+++ src/getpid.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <unistd.h>
+
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+
+int main(int argc, const char** argv)
+{
+	std::vector<std::string> terms;
+
+	for(int i = 1; i < argc; i++)
+		terms.push_back(std::string(argv[i]));
+
+	int pid = getpid();
+	const auto pid_string = std::to_string(pid);
+
+	const auto proc_path = std::filesystem::path("/proc/");
+	for (const auto& entry : std::filesystem::directory_iterator(proc_path))
+	{
+		if (!entry.is_directory())
+			continue;
+		
+		const auto entry_path = entry.path();
+		const auto entry_filename = entry_path.filename().string();
+		
+		if (entry_filename == pid_string)
+			continue;
+
+		if (entry_filename.find("self") != entry_filename.npos
+			|| entry_filename.find("thread_self") != entry_filename.npos)
+			continue;
+		
+		const auto command_line_file = entry_path / "cmdline";
+		auto file_stream = std::ifstream(command_line_file.string());
+		
+		auto string_stream = std::stringstream();
+		string_stream << file_stream.rdbuf();
+		const auto contents = string_stream.str();
+		
+		for(const auto& term : terms)
+			if (contents.find(term) != contents.npos)
+				std::puts(entry_filename.c_str());
+	}
+}
+
+
---
+++ src/modhost.cpp
@@ -0,0 +1,65 @@
+
+#include <cstdio>
+
+#include <fstream>
+#include <ios>
+#include <sstream>
+#include <string>
+#include <vector>
+
+const auto host_file_path = "/etc/hosts";
+// const auto host_file_path = "hosts";
+
+void remove_hostlines()
+{
+	std::vector<std::string> new_lines;
+	{
+		auto host_file = std::ifstream(host_file_path);
+
+		std::string current_line;
+		while(std::getline(host_file, current_line))
+		{
+			if (current_line.find("#automated") == current_line.npos)
+				new_lines.push_back(current_line);
+		}
+	}
+	{
+		auto host_file = std::ofstream(host_file_path, std::ios_base::trunc);
+		for(const auto& line : new_lines)
+			host_file << line << std::endl;
+	}
+}
+
+void add_hostlines(std::vector<std::string> lines)
+{
+	auto host_file = std::ofstream(host_file_path, std::ios_base::app);
+	for(const auto& line : lines)
+	{
+		host_file << std::endl << (line + " #automated");
+	}
+}
+
+int main(int argc, const char** argv)
+{
+	char operation = argv[1][0];
+
+	if (operation == 'a')
+	{
+		std::vector<std::string> lines;
+
+		for(int i = 2; i < argc; i++)
+			lines.push_back(std::string(argv[i]));
+
+		add_hostlines(lines);
+	}
+	else if (operation == 'd')
+	{
+		remove_hostlines();
+	}
+	else 
+	{
+		std::fprintf(stderr, "Unknown operation");
+	}
+}
+
+
