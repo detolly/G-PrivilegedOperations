20afe0d2b5cfa6ca06a4636de05d3e833c8e85ca
detolly <thomas@detolly.no>
2024-07-25 02:55:04 +0200

    i love over-engineering

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-clang++ -O3 getmem.cpp -o bin/getmem -std=c++23 &
-clang++ -O3 getpid.cpp -o bin/getpid -std=c++23 &
-clang++ -O3 modhost.cpp -o bin/modhost -std=c++23 &
+clang++ -O3 getmem.cpp -o bin/getmem -std=c++23 -Werror -Wall -Wpedantic -Wconversion &
+clang++ -O3 getpid.cpp -o bin/getpid -std=c++23 -Werror -Wall -Wpedantic -Wconversion &
+clang++ -O3 modhost.cpp -o bin/modhost -std=c++23 -Werror -Wall -Wpedantic -Wconversion &
 
 wait
 
--- getmem.cpp
+++ getmem.cpp
@@ -19,11 +19,8 @@
 
 using namespace ctre::literals;
 
-constexpr static auto RC4_RAW_TABLE_ELEMENT_SIZE = 8;
 constexpr static auto RC4_TABLE_SIZE = 256;
 constexpr static auto RC4_INVALID_MASK_SHOCKWAVE = 0xFFFFFFFB'FFFFFF00;
-
-constexpr static auto RC4_RAW_TABLE_SIZE = RC4_RAW_TABLE_ELEMENT_SIZE * RC4_TABLE_SIZE;
 
 struct map
 {
@@ -33,6 +30,20 @@ struct map
     std::uint64_t size() const { return end - start; }
 };
 
+struct rc4_entry_raw
+{
+    union {
+        std::uint64_t big;
+        std::uint32_t medium[2];
+        std::uint8_t small[8];
+    } value;
+
+};
+
+static_assert(sizeof(rc4_entry_raw) == 8);
+
+using rc4_table = std::array<std::uint8_t, RC4_TABLE_SIZE>;
+using rc4_table_raw = std::array<rc4_entry_raw, RC4_TABLE_SIZE>;
 
 std::vector<map> get_process_maps(pid_t pid)
 {
@@ -64,7 +75,7 @@ std::vector<map> get_process_maps(pid_t pid)
     return maps;
 }
 
-void print_table(const std::array<std::uint8_t, RC4_TABLE_SIZE>& table)
+void print_table(const rc4_table& table)
 {
     for(const auto entry : table)
         printf("%02x", entry);
@@ -72,47 +83,48 @@ void print_table(const std::array<std::uint8_t, RC4_TABLE_SIZE>& table)
     printf("\n");
 }
 
-constexpr std::optional<std::array<std::uint8_t, RC4_TABLE_SIZE>> extract_table(const std::span<const std::uint8_t> table_span)
+constexpr std::optional<rc4_table> extract_table(const rc4_table_raw& raw_table)
 {
-    auto table = std::array<std::uint8_t, RC4_TABLE_SIZE>{ 0 };
+    auto table = rc4_table{ 0 };
     auto values_already_seen = std::array<bool, RC4_TABLE_SIZE>{ false };
 
-    if (table_span.size() != RC4_RAW_TABLE_SIZE)
-        return {};
-
-    for(auto i = 0uz; i < table_span.size(); i += RC4_RAW_TABLE_ELEMENT_SIZE)
+    for(auto i = 0uz; i < raw_table.size(); i++)
     {
-        const auto extracted_value = table_span[i];
+        const auto extracted_value = raw_table[i].value.small[0];
         if (values_already_seen[extracted_value])
             return {};
 
-        table[i / RC4_RAW_TABLE_ELEMENT_SIZE] = extracted_value;
+        table[i] = extracted_value;
         values_already_seen[extracted_value] = true;
     }
 
     return table;
 }
 
-constexpr std::vector<std::array<std::uint8_t, RC4_TABLE_SIZE>> check_map_tables(const std::uint64_t offset, const std::span<const std::uint8_t> buffer)
+constexpr std::vector<rc4_table> check_map_tables(const std::span<const rc4_entry_raw> buffer)
 {
-    std::vector<std::array<std::uint8_t, RC4_TABLE_SIZE>> tables;
+    std::vector<rc4_table> tables;
 
-    auto valid_entries = 0;
-    for(auto i = offset; i < buffer.size(); i += RC4_RAW_TABLE_ELEMENT_SIZE)
+    if (buffer.size() < RC4_TABLE_SIZE)
+        return tables;
+
+    auto start = buffer.begin();
+
+    for(auto it = buffer.begin(); it != buffer.end(); it++)
     {
-        const auto value = *reinterpret_cast<const std::uint64_t*>(buffer.data() + i);
-        if ((value & RC4_INVALID_MASK_SHOCKWAVE) != 0) {
-            valid_entries = 0;
-            continue;
+        const auto& entry = *it;
+
+        if ((entry.value.big & RC4_INVALID_MASK_SHOCKWAVE) != 0) {
+            start = it + 1;
         }
 
-        valid_entries++;
-        if (valid_entries == RC4_TABLE_SIZE) {
-            const auto maybe_table = extract_table({ buffer.data() + i - RC4_RAW_TABLE_SIZE + RC4_RAW_TABLE_ELEMENT_SIZE, RC4_RAW_TABLE_SIZE });
+        else if (it - start == RC4_TABLE_SIZE) {
+            const auto& current_table = reinterpret_cast<const rc4_table_raw&>(*start);
+            const auto maybe_table = extract_table(current_table);
             if (maybe_table.has_value()) {
                 tables.push_back(maybe_table.value());
             }
-            valid_entries--;
+            start++;
         }
     }
 
@@ -121,31 +133,31 @@ constexpr std::vector<std::array<std::uint8_t, RC4_TABLE_SIZE>> check_map_tables
 
 void check_map(const pid_t pid, const map m)
 {
-    std::uint8_t* const buffer = new std::uint8_t[m.size()]{ 0 };
+    std::vector<std::uint8_t> buffer;
+    buffer.resize(m.size());
 
-    iovec local{ .iov_base = buffer, .iov_len = m.size() };
+    iovec local{ .iov_base = buffer.data(), .iov_len = m.size() };
     iovec remote{ .iov_base = reinterpret_cast<void*>(m.start), .iov_len = m.size() };
 
-    auto rc = process_vm_readv(pid, &local, 1, &remote, 1, 0);
-    if (rc < 0) {
-        // perror("Failed to read memory");
-        // fprintf(stderr, "\tat %08x\n", static_cast<std::uint32_t>(m.start));
-        delete[] buffer;
+    const auto rc = process_vm_readv(pid, &local, 1, &remote, 1, 0);
+    if (rc < 0)
         return;
-    }
 
-    constexpr auto check_and_print_tables_with_offset = [](const auto* const buffer, const std::uint64_t offset, const map _map) {
-        for(const auto& table : check_map_tables(offset, { buffer, _map.size() }))
+    constexpr auto check_and_print_tables_with_offset = [](const auto& buffer, const std::size_t offset) {
+        const auto span = std::span<const rc4_entry_raw> {
+            reinterpret_cast<const rc4_entry_raw*>(buffer.data() + offset), 
+            (buffer.size() - offset) / sizeof(rc4_entry_raw) 
+        };
+
+        for(const auto& table : check_map_tables(span))
             print_table(table);
     };
 
     // for(auto i = 0uz; i < RC4_RAW_TABLE_ELEMENT_SIZE; i++)
-    //     check_and_print_tables_with_offset(buffer, i, m);
+    //     check_and_print_tables_with_offset(buffer, i);
 
-    check_and_print_tables_with_offset(buffer, 0, m);
-    check_and_print_tables_with_offset(buffer, 4, m);
-
-    delete[] buffer;
+    check_and_print_tables_with_offset(buffer, 0);
+    check_and_print_tables_with_offset(buffer, 4);
 }
 
 int main(int argc, const char** argv)
